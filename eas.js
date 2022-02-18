7be7202234a7a6eb05a57b30c218584291d89efa
Ismael Valentin <ismaelvale@gmail.com>
2022-02-18 13:17:35 -0400

    Added sticky background eventListener to squares

--- eas.js
+++ eas.js
@@ -3,5 +3,11 @@ const container = document.querySelector('div');
 const div = document.createElement('div');
 
 for (let i = 0; i < 256; i++){
-    container.innerHTML += '<div class="square"></div>';
+    container.innerHTML += '<div id="square"></div>';
 }
+
+document.addEventListener("mouseover", function(e) {
+    if (e.target && e.target.id== 'square') {
+        e.target.style.backgroundColor= 'black';
+    }
+});
