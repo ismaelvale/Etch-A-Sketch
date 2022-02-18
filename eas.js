b2e24753eef5d7a41f0f0fd3b03cd21fe930cb95
Ismael Valentin <ismaelvale@gmail.com>
2022-02-18 18:53:50 -0400

    back to normal

--- eas.js
+++ eas.js
@@ -2,13 +2,12 @@ const body = document.body;
 const container = document.querySelector('div');
 const div = document.createElement('div');
 
-window.onload = createGrid (256);
-
-function createGrid (n) {
-    for (let i = 0; i < n; i++){
+window.onload = function createGrid () {
+    for (let i = 0; i < 256; i++){
         container.innerHTML += '<div id="square"></div>';
     }
 }
+
 
 document.addEventListener("mouseover", function(e) {
     if (e.target && e.target.id== 'square') {
@@ -21,5 +20,5 @@ const refresh = document.querySelector('button');
 refresh.addEventListener('click', function() {
     const grid = prompt('Choose grid size: (Max:100)');
     console.log(grid);
-    createGrid(grid);
+
 }); 
